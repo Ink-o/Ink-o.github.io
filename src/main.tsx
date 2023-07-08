f11f698c425872e6453a5d4206ad6a3e47fe51bb
ink <jiacheng.qiu@shopcider.com>
2023-07-08 15:07:14 +0800

    feat: 页面功能初始化

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <div>你好，世界</div>
+  </React.StrictMode>,
+)
