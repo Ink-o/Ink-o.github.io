51cc4afdc04172ad06e369d3fce808c973d1bd1d
ink <jiacheng.qiu@shopcider.com>
2023-07-08 19:54:09 +0800

    feat: 项目初始化

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div>你好，世界</div>
+    <div>你好，世界啊啊啊啊啊啊</div>
   </React.StrictMode>,
 )
